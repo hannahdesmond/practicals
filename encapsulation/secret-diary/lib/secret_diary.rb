86c8425efc1a1c173db089c138d7b0b450ae819e
Hannah Desmond <desmond.hannah@googlemail.com>
2021-12-09 11:52:51 +0000

    Add new .NewEntry class

--- encapsulation/secret-diary/lib/secret_diary.rb
+++ encapsulation/secret-diary/lib/secret_diary.rb
@@ -7,14 +7,6 @@ class SecretDiary
     @entries = []
   end
 
-  def add_entry(string)
-    @locked == false ? @entries << string : "This diary is locked. Keep out!"
-  end
-
-  def get_entries
-    @locked == false ? @entries : "This diary is locked. Keep out!"  
-  end
-
   def unlock
     @locked = false
   end
@@ -22,5 +14,21 @@ class SecretDiary
   def lock
     @locked = true
   end
-
 end
+
+class NewEntry
+  def add_entry(diary, string)
+    diary.locked == false ? diary.entries << string : "This diary is locked. Keep out!"
+  end
+end
+
+
+=begin
+  def add_entry(string)
+    @locked == false ? @entries << string : "This diary is locked. Keep out!"
+  end
+
+  def get_entries
+    @locked == false ? @entries : "This diary is locked. Keep out!"  
+  end
+=end
--- encapsulation/secret-diary/spec/secret_diary_spec.rb
+++ encapsulation/secret-diary/spec/secret_diary_spec.rb
@@ -2,13 +2,27 @@ require 'secret_diary'
 
 describe SecretDiary do
   let(:secretdiary) { SecretDiary.new }
-  before do
-    secretdiary.locked = true
-  end
-  context 'is locked' do
-    it 'should return true' do
+    it 'unlocks' do
+      secretdiary.locked = true
+      secretdiary.unlock
+      expect(secretdiary.locked).to eq(false)
+    end
+    it 'locks the diary' do
+      secretdiary.lock
       expect(secretdiary.locked).to eq(true)
     end
+  end
+
+describe NewEntry do
+  let(:secretdiary) { SecretDiary.new }
+  it 'should add an entry' do
+    secretdiary.unlock
+    subject.add_entry(secretdiary, "nice note")
+    expect(secretdiary.entries).to eq(["nice note"])
+  end
+end
+
+=begin
     it 'should throw an error at #add_entry' do
       expect(secretdiary.add_entry("hello")).to eq("This diary is locked. Keep out!")  
     end
@@ -20,21 +34,14 @@ describe SecretDiary do
     before do
       secretdiary.unlock
     end
-    it 'unlocks' do
-      expect(secretdiary.locked).to eq(false)
-    end
-    it 'should add an entry' do
-      secretdiary.add_entry("nice note")
-      expect(secretdiary.entries).to eq(["nice note"])
-    end
+    
+    
     it 'should get the entries' do
       secretdiary.add_entry("nice note")
       secretdiary.add_entry("another note")
       expect(secretdiary.get_entries).to eq(["nice note", "another note"])
     end
-    it 'locks the diary' do
-      secretdiary.lock
-      expect(secretdiary.locked).to eq(true)
-    end
+    
   end
 end
+=end
