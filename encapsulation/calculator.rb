7576f866afd83a8b4639889176d96dbdc8b6c3d3
Hannah Desmond <desmond.hannah@googlemail.com>
2021-12-14 10:27:58 +0000

    Add #show_answer method to calculator class

--- encapsulation/calculator.rb
+++ encapsulation/calculator.rb
@@ -6,6 +6,12 @@ class Calculator
   def subtract(number_1, number_2)
     number_1 - number_2
   end
+
+  def show_answer(result)
+    printer = Printer.new
+    printer.print_answer(result)
+  end
+
 end
 
 class Printer
